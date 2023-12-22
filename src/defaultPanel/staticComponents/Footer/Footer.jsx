3289455256b46a6043c9af4a22b0d2a19e99e3b8
Korneliia <korneliiaPositive@gmail.com>
2023-12-22 19:42:37 +0100

    create generate-react-cli.json

--- src/defaultPanel/staticComponents/Footer/Footer.jsx
+++ src/defaultPanel/staticComponents/Footer/Footer.jsx
@@ -1,6 +1,14 @@
-const Footer = ()=>{
-    return(
-        <footer>footer
+import style from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className={style.topPartFooter}>
+                top
+            </div>
+            <div className={style.bottomPartFooter}>
+                bottom
+            </div>
         </footer>
     )
 }
---
+++ template/TemplateName.jsx
@@ -0,0 +1,10 @@
+import style from "./TemplateName.module.css";
+
+const TemplateName = () => {
+    return (
+        <div className={style.container}>
+
+        </div>
+    )
+}
+export default TemplateName;
