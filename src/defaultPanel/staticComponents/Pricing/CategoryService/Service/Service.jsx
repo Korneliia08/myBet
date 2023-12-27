7b4c15e6bc2f63446b35e3a8ff135a60a55f4558
Korneliia <korneliiaPositive@gmail.com>
2023-12-27 16:54:17 +0200

    change in "pricing" component

--- src/defaultPanel/staticComponents/Pricing/CategoryService/Service/Service.jsx
+++ src/defaultPanel/staticComponents/Pricing/CategoryService/Service/Service.jsx
@@ -4,8 +4,8 @@ const Service = () => {
     return (
         <div className={style.containerForService}>
             <div className={style.blockForTitlePrice}>
-                <h4>Hair Cut</h4>
-                <span>40$</span>
+                <h4 className={style.titleService}>Hair Cut</h4>
+                <span className={style.price}>40$</span>
             </div>
             <p className={style.describe}>Barber is a person whose occupation is mainly to cut dress groom style and
                 shave men.</p>
