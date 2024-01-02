be7309e0c4e8af4b946bb78521a0d8a27e399ea2
Korneliia <korneliiaPositive@gmail.com>
2024-01-02 18:10:55 +0100

    create registration

--- src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseEmpoloyee/CardOfEmployee/CardOfEmployee.jsx
+++ src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseEmpoloyee/CardOfEmployee/CardOfEmployee.jsx
@@ -1,9 +1,14 @@
 import style from "./CardOfEmployee.module.css";
+import image from "../../../../../assets/images/employee1.jpg";
 
 const CardOfEmployee = () => {
     return (
         <div className={style.container}>
-            CardOfEmployee
+            <img src={image} alt="image of employee"/>
+            <div className={style.rightPart}>
+                <h5>Matthew Phillips</h5>
+                <button>Choose</button>
+            </div>
         </div>
     )
 }
