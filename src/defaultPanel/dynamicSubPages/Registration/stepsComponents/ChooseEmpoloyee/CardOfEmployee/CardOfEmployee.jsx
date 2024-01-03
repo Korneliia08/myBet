f654cb3201a05d7861fcb6243a4a4a01252c6d17
Korneliia <korneliiaPositive@gmail.com>
2024-01-03 19:03:50 +0100

    card of employee

--- src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseEmpoloyee/CardOfEmployee/CardOfEmployee.jsx
+++ src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseEmpoloyee/CardOfEmployee/CardOfEmployee.jsx
@@ -14,6 +14,15 @@ const CardOfEmployee = () => {
             <img src={image} alt="image of employee"/>
             <div className={style.rightPart}>
                 <h5>Matthew Phillips</h5>
+                <div className={style.containerAllDays}>
+                    <span>Mo</span>
+                    <span className={style.noActive}>Tu</span>
+                    <span>We</span>
+                    <span>Th</span>
+                    <span>Fr</span>
+                    <span className={style.noActive}>St</span>
+                    <span>Sn</span>
+                </div>
                 <button onClick={desplayChooseAService}>Choose</button>
             </div>
         </div>
