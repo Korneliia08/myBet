95896c4847ff05410df745971d2eee4b9f8897c7
Korneliia <korneliiaPositive@gmail.com>
2024-01-04 14:51:27 +0100

    create component "veryfication"

--- src/defaultPanel/dynamicSubPages/Registration/stepsComponents/Veryfication/Veryfication.jsx
+++ src/defaultPanel/dynamicSubPages/Registration/stepsComponents/Veryfication/Veryfication.jsx
@@ -1,9 +1,17 @@
+import Back from "../../../../components/Back/Back";
+import Moustache from "../../../../components/Moustache/Moustache";
 import style from "./Veryfication.module.css";
 
 const Veryfication = () => {
     return (
-        <div className={style.container}>
-            Veryfication
+        <div className="containerInRegistration">
+            <Back/>
+            <h4 className="mainTitle">Verification</h4>
+            <Moustache/>
+            <form className={style.form}>
+                <input type="text" placeholder="veryfication code"/>
+                <input type="submit" value="Next" className="nextBtn"/>
+            </form>
         </div>
     )
 }
