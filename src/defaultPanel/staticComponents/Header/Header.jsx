a7b17c250e1295326dee46042ac22a38d8e48cc4
Korneliia <korneliiaPositive@gmail.com>
2024-01-04 16:44:09 +0100

    add "if" to header component

--- src/defaultPanel/staticComponents/Header/Header.jsx
+++ src/defaultPanel/staticComponents/Header/Header.jsx
@@ -1,24 +1,28 @@
 import Nav from "./Nav/Nav";
 import style from "./Header.module.css";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 const Header = () => {
     const navigate = useNavigate();
+
+    const location = useLocation();
 
     function displayRegistration() {
         navigate("/registration");
     }
 
+    console.log(location)
     return (
         <header className={style.header}>
             <Nav/>
-            <div className={style.container}>
+            {location.pathname == "/" ? <div className={style.container}>
                 <h1 className={style.title}>Enjoy our barber services</h1>
                 <p className={style.content}>Establish your new amazing look with Barbershop services! From haircuts to
                     hot shaves, you can
                     experience the best level of barbering in the area.</p>
                 <button className={style.btnBookNow} onClick={displayRegistration}>Book now</button>
-            </div>
+            </div> : ""}
+
         </header>
     )
 }
