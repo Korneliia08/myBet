a258c3f1b0ef28a0ccd2d2796ef22dada0699ef7
Korneliia <korneliiaPositive@gmail.com>
2024-01-04 15:14:03 +0100

    create component "summary"

--- src/defaultPanel/dynamicSubPages/Registration/stepsComponents/Veryfication/Veryfication.jsx
+++ src/defaultPanel/dynamicSubPages/Registration/stepsComponents/Veryfication/Veryfication.jsx
@@ -16,6 +16,11 @@ const Veryfication = () => {
             <Back/>
             <h4 className="mainTitle">Verification</h4>
             <Moustache/>
+            <p className="contentInRegistration">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis deleniti harum quis temporibus
+                voluptates! Aliquid corporis debitis doloribus esse facere hic ipsum maiores necessitatibus, optio
+                sapiente!
+            </p>
             <form className={style.form}>
                 <input type="text" placeholder="veryfication code"/>
                 <input type="submit" value="Next" className="nextBtn" onClick={desplaySummary}/>
