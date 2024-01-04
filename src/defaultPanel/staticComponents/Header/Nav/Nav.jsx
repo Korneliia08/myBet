e8390a0e943a2b086ff0a45554c1b16b376c07d2
Korneliia <korneliiaPositive@gmail.com>
2024-01-04 16:55:22 +0100

    made the navigation fixed at the top

--- src/defaultPanel/staticComponents/Header/Nav/Nav.jsx
+++ src/defaultPanel/staticComponents/Header/Nav/Nav.jsx
@@ -3,19 +3,24 @@ import {Link} from "react-router-dom";
 
 const Nav = () => {
     return (
-        <div className={style.containerForTitleAndNav}>
-            <h1 className={style.title}>Barbersh<span>o</span>p demo</h1>
-            <nav className={style.nav}>
-                <Link to={"/registration"}>registration</Link>
-                <Link to={"/"}>employees</Link>
-                <Link to={"/"}>about us</Link>
-                <Link to={"/"}>services</Link>
-                <Link to={"/"}>pricing</Link>
-                <Link to={"/"}>contact</Link>
-                <Link to={"/rgdev"}>map</Link>
-                <Link to={"/rgdev"} className={style.btnWriteToUs}>write to us</Link>
-            </nav>
-        </div>
+        <>
+            <div className={style.emptyBlock}>
+                s
+            </div>
+            <div className={style.containerForTitleAndNav}>
+                <h1 className={style.title}>Barbersh<span>o</span>p demo</h1>
+                <nav className={style.nav}>
+                    <Link to={"/registration"}>registration</Link>
+                    <Link to={"/"}>employees</Link>
+                    <Link to={"/"}>about us</Link>
+                    <Link to={"/"}>services</Link>
+                    <Link to={"/"}>pricing</Link>
+                    <Link to={"/"}>contact</Link>
+                    <Link to={"/rgdev"}>map</Link>
+                    <Link to={"/rgdev"} className={style.btnWriteToUs}>write to us</Link>
+                </nav>
+            </div>
+        </>
     )
 }
 export default Nav;
