df8fa799d650a80a31baa6fb53b710fbb709009a
Korneliia <korneliiaPositive@gmail.com>
2024-01-18 21:07:03 +0200

    add style to selected day

--- src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseDate/DateCard/Day/Day.jsx
+++ src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseDate/DateCard/Day/Day.jsx
@@ -1,9 +1,11 @@
 import style from "./Day.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setData } from "../../../../../../../data/reducers/registrationReducer";
+import { useEffect, useState } from "react";
 
 const Day = (props) => {
   const dispatch = useDispatch();
+  const [selected, setSelected] = useState(false);
   const data = props.data;
   const dayOfMonth = data.getDate();
   const dayOfWeek = [
@@ -16,13 +18,24 @@ const Day = (props) => {
     "Saturday",
   ];
   const titleOfDay = dayOfWeek[data.getDay()];
+  const selectedDay = useSelector((state) => state.registration.selectedDay);
+
+  useEffect(() => {
+    if (selectedDay !== "") {
+      const day = selectedDay.getDate();
+      setSelected(day === new Date(data).getDate());
+    }
+  }, [selectedDay]);
 
   function desplayHours() {
     dispatch(setData({ selectedDay: data }));
   }
 
   return (
-    <div className={style.blockDay} onClick={desplayHours}>
+    <div
+      className={`${style.blockDay} ${selected ? style.selectedDay : ""}`}
+      onClick={desplayHours}
+    >
       <span className={style.number}>{dayOfMonth}</span>
       <span className={style.day}>{titleOfDay}</span>
     </div>
