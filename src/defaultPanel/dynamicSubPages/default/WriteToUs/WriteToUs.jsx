4892bfc4194ea0847d7defa0f98ab92aced34f9e
Korneliia <korneliiaPositive@gmail.com>
2024-01-20 19:15:37 +0200

    validation in form "write to us"

--- src/defaultPanel/dynamicSubPages/default/WriteToUs/WriteToUs.jsx
+++ src/defaultPanel/dynamicSubPages/default/WriteToUs/WriteToUs.jsx
@@ -2,9 +2,11 @@ import style from "./WriteToUs.module.css";
 import backgroundImage from "../../../assets/images/backgroundWriteToUs.jpg";
 import Moustache from "../../../components/Moustache/Moustache";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const WriteToUs = () => {
   const data = useSelector((state) => state.stateOfPage.stateOfPage.getInTouch);
+  const [invalidStyle, setInvalidStyle] = useState(false);
   return (
     <section className={style.container}>
       <img
@@ -16,19 +18,24 @@ const WriteToUs = () => {
         <h2 className="mainTitle">{data.title}</h2>
         <Moustache />
         <p className={style.content}>{data.shortDescription}</p>
-        <form>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Surname" />
-          <input type="email" placeholder="E-mail" />
+        <form className={invalidStyle ? style.invalidStyle : ""}>
+          <input type="text" placeholder="Name" required="true" />
+          <input type="text" placeholder="Surname" required="true" />
+          <input type="email" placeholder="E-mail" required="true" />
           <div className={style.groupe}>
-            <select>
+            <select required="true">
               <option value="+48">+48</option>
               <option value="+380">+380</option>
             </select>
-            <input type="text" placeholder="Phone" />
+            <input type="text" placeholder="Phone" required="true" />
           </div>
-          <textarea placeholder="Message"></textarea>
-          <input type="submit" value="send request" className={style.submit} />
+          <textarea placeholder="Message" required="true"></textarea>
+          <input
+            type="submit"
+            value="send request"
+            className={style.submit}
+            onClick={() => setInvalidStyle(true)}
+          />
         </form>
       </div>
     </section>
