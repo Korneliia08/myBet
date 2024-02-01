bff2c6d32e9a3e5266dc44194440ca72560f7a21
Korneliia <korneliiaPositive@gmail.com>
2024-02-01 14:58:07 +0100

    responsive

--- src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseDate/ChooseDate.jsx
+++ src/defaultPanel/dynamicSubPages/Registration/stepsComponents/ChooseDate/ChooseDate.jsx
@@ -3,11 +3,33 @@ import Moustache from "../../../../components/Moustache/Moustache";
 import style from "./ChooseDate.module.css";
 import DateCard from "./DateCard/DateCard";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const ChooseDate = () => {
   const dataDefault = useSelector(
     (state) => state.stateOfPage.stateOfPage.registration.steps.chooseAdate,
   );
+
+  const navigate = useNavigate();
+  const idOfEmployee = useSelector((state) => state.registration.idOfEmployee);
+  const idOfServices = useSelector((state) => state.registration.idOfServices);
+  useEffect(() => {
+    if (idOfServices.length) {
+      toast.warn("Registration back to first step\n No service selected", {
+        allowHtml: true,
+      });
+      navigate("/registration");
+    }
+
+    if (idOfEmployee === 0) {
+      toast.warn("Registration back to first step</br>No employee selected", {
+        allowHtml: true,
+      });
+      navigate("/registration");
+    }
+  }, [idOfEmployee]);
   return (
     <div className="containerInRegistration">
       <Back />
